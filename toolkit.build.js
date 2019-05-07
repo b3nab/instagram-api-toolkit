8dbafe9e0ed565b03b2587aa16fed2acd983141a
Ben <b3nab@users.noreply.github.com>
2019-05-07 23:13:11 +0200

    init toolkit

---
+++ toolkit.build.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _inquirer = _interopRequireDefault(require("inquirer"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var ascii_art = "\n                            __  . .* ,\n                          ~#@#%(\" .,$ @\n                          .\"^ ';\"\n                        ..\n                        ;. :                                   . .\n                        ;==:                     ,,   ,.@#(&*.;'\n                        ;. :                   .;#$% & ^^&\n                        ;==:                   &  ......\n                        ;. :                   ,,;      :\n                        ;==:  ._______.       ;  ;      :\n                        ;. :  ;    ###:__.    ;  ;      :\n  _____________________.'  `._;       :  : __.' .'        `.__________________\n  #############################################################################\n  ########################## Instagram API Toolkit ############################\n  #############################################################################\n";
+
+var wizard = function wizard() {
+  // define all the command that this wizard can do
+  console.log(ascii_art); // helper
+  // new config
+  // new sdk
+};
+
+wizard();
