79893efadc7182156ceb73a56bd74b78155e4ee3
Ben <b3nab@users.noreply.github.com>
2019-05-08 21:52:02 +0200

    update example to login with instagram_private_sdk

--- test/python-sdk.py
+++ test/python-sdk.py
@@ -1,17 +1,47 @@
 from __future__ import print_function
 import time
+import hmac
+import json
+import hashlib
+import uuid as uuid_library
+import six.moves.urllib as urllib
 import private_instagram_sdk
 from private_instagram_sdk.rest import ApiException
 from pprint import pprint
 
+USERNAME, PASSWORD = 'username', 'password'
+IG_SIG_KEY = '99e16edcca71d7c1f3fd74d447f6281bd5253a623000a55ed0b60014467a53b1'
+
+def hex_digest(*args):
+    m = hashlib.md5()
+    m.update(b''.join([arg.encode('utf-8') for arg in args]))
+    return m.hexdigest()
+
+def generate_device_id(seed):
+    volatile_seed = "12345"  # Important ! :) :)
+    m = hashlib.md5()
+    m.update(seed.encode('utf-8') + volatile_seed.encode('utf-8'))
+    return 'android-' + m.hexdigest()[:16]
+
+phone_id = str(uuid_library.uuid4())
+uuid = str(uuid_library.uuid4())
+device_id = generate_device_id(hex_digest(USERNAME, USERNAME))
+data = json.dumps({
+    'phone_id': phone_id,
+    'device_id': device_id,
+    'guid': uuid,
+    'username': USERNAME,
+    'password': PASSWORD,
+})
+
 # create an instance of the API class
 api_instance = private_instagram_sdk.AuthApi(private_instagram_sdk.ApiClient())
-username = 'username'  # str | the username (or nickname)  (optional)
-password = 'password'  # str | the password to use for authentication  (optional)
+ig_sig_key_version = 4  # int |  (optional)
+signed_body = hmac.new(IG_SIG_KEY.encode('utf-8'), data.encode('utf-8'), hashlib.sha256).hexdigest() + '.' + urllib.parse.quote(data)  # str |  (optional)
 
 try:
     # Login user to Instagram
-    api_response = api_instance.accounts_login_post(username=username, password=password)
+    api_response = api_instance.accounts_login_post(ig_sig_key_version=ig_sig_key_version, signed_body=signed_body)
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling AuthApi->accounts_login_post: %s\n" % e)
