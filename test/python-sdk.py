68e7b6c79cfa1f37b3d0516273edecbad51a2da9
Ben <b3nab@users.noreply.github.com>
2019-05-09 18:59:17 +0200

    add user-agent generation in test for python-sdk

--- test/python-sdk.py
+++ test/python-sdk.py
@@ -34,14 +34,36 @@ data = json.dumps({
     'password': PASSWORD,
 })
 
+DEVICE = {
+    'instagram_version': '26.0.0.10.86',
+    'android_version': 24,
+    'android_release': '7.0',
+    'dpi': '640dpi',
+    'resolution': '1440x2560',
+    'manufacturer': 'HUAWEI',
+    'device': 'LON-L29',
+    'model': 'HWLON',
+    'cpu': 'hi3660'
+}
+
+USER_AGENT_BASE = (
+    'Instagram {instagram_version} '
+    'Android ({android_version}/{android_release}; '
+    '{dpi}; {resolution}; {manufacturer}; '
+    '{device}; {model}; {cpu}; en_US)'
+)
+
+user_agent = USER_AGENT_BASE.format(**DEVICE)  # just insert params
+
 # create an instance of the API class
 api_instance = private_instagram_sdk.AuthApi(private_instagram_sdk.ApiClient())
+# user_agent = 'user_agent_example'  # str | the User-Agent used by the Instagram App (be carefull in generation of a new one) 
 ig_sig_key_version = 4  # int |  (optional)
 signed_body = hmac.new(IG_SIG_KEY.encode('utf-8'), data.encode('utf-8'), hashlib.sha256).hexdigest() + '.' + urllib.parse.quote(data)  # str |  (optional)
 
 try:
     # Login user to Instagram
-    api_response = api_instance.accounts_login_post(ig_sig_key_version=ig_sig_key_version, signed_body=signed_body)
+    api_response = api_instance.accounts_login_post(user_agent, ig_sig_key_version=ig_sig_key_version, signed_body=signed_body)
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling AuthApi->accounts_login_post: %s\n" % e)
